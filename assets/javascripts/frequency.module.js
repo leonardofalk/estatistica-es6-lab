f4970dc596a12b9b28993b7efc20c73827cfe32a
Leonardo Falk <leonardo.falk@hotmail.com>
2017-06-07 00:14:18 -0300

    Distributed frequency table

--- assets/javascripts/frequency.module.js
+++ assets/javascripts/frequency.module.js
@@ -1,40 +1,70 @@
 import 'bootstrap-sass/assets/javascripts/bootstrap';
 
+$('[data-toggle=tooltip]').tooltip();
+
 export function frequencyModule(vals) {
-  let media = vals.reduce((a, b) => { return a + b });
-  let total = 0, totald = 0, totald2 = 0;
+  let totalSum = vals.reduce((a, b) => { return a + b });
+  let media = totalSum / vals.length;
   let intervals = calcIntervals(vals);
 
   let tableInfo = intervals.map((interval) => {
-    let min = interval.min, max = interval.max;
+    let min = interval.min, max = interval.max;;
 
     return {
-      interval: sprintf('%d ├── %d', min, max),
+      interval: sprintf('%03d ├─ %03d', min, max),
       frequency: vals.map((n) => {
         return (n <= max && n >= min) ? 1 : 0;
       }).reduce((a, b) => {
         return a + b;
-      }),
+      })
     };
   });
 
-  tableInfo[tableInfo.length] = {
-    interval: '-',
-    frequency: tableInfo.map((num) => {
-      return num.frequency;
-    }).reduce((a, b) => { return a + b; })
-  }
+  let totalFrequency = tableInfo.map((num) => {
+    return num.frequency;
+  }).reduce((a, b) => { return a + b; });
+  let acumulatedFrequency = 0;
+
+  tableInfo.map((e) => {
+    let frequencyPercent = Math.min(e.frequency * 100 / totalFrequency, 100.0);
+    acumulatedFrequency += e.frequency;
+    let acumulatedFrequencyPercent = Math.min(acumulatedFrequency * 100 / totalFrequency, 100);
+
+    return $.extend(e, {
+      frequencyPercent: frequencyPercent,
+      acumulatedFrequency: acumulatedFrequency,
+      acumulatedFrequencyPercent: acumulatedFrequencyPercent
+    });
+  });
 
   let tableHTML = [];
 
   for (let tableRow of tableInfo) {
     tableHTML.push(sprintf(`
     <tr>
-      <td>%s</td>
+      <td>%3s</td>
+      <td>%3d</td>
+      <td>%.2f</td>
       <td>%d</td>
+      <td>%.2f</td>
     </tr>
-    `, tableRow.interval, tableRow.frequency));
+    `, tableRow.interval,
+       tableRow.frequency,
+       tableRow.frequencyPercent,
+       tableRow.acumulatedFrequency,
+       tableRow.acumulatedFrequencyPercent
+    ));
   }
+
+  tableHTML.push(sprintf(`
+    <tr>
+      <td><b>Total</b> %d</td>
+      <td>%d</td>
+      <td>%.2f</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+  `, totalSum, totalFrequency, 100.0));
 
   document.getElementById('frequency_table').innerHTML = tableHTML.join("\n");
 }
--- assets/javascripts/input.js
+++ assets/javascripts/input.js
@@ -3,8 +3,10 @@ import { frequencyModule } from './frequency.module';
 import { chartModule } from './chart.module';
 
 for (let ev of ['change', 'keyup', 'keydown']) {
-  document.getElementById('entrada').addEventListener(ev, function() {
-    let inputVals = this.value.trim().split(/\s+/).map(Number);
+  let $entrada = document.getElementById('entrada');
+
+  $entrada.addEventListener(ev, () => {
+    let inputVals = $entrada.value.trim().split(/\s+/).map(Number);
     let sortedVals = inputVals.slice(0).sort((a, b) => {
       return parseInt(a || 0, 10) - parseInt(b || 0, 10);
     });
@@ -16,12 +18,29 @@ for (let ev of ['change', 'keyup', 'keydown']) {
 }
 
 document.getElementById('example_run').addEventListener('click', () => {
+  let maxCount = parseInt(document.getElementById('random_amount').value, 10);
+  let maxRandRange = parseInt(document.getElementById('max_random_range').value, 10);
+  let minRandRange = parseInt(document.getElementById('min_random_range').value, 10);
+
+  if (isNaN(maxCount))     { maxCount = 6; }
+  if (isNaN(maxRandRange)) { maxRandRange = 100; }
+  if (isNaN(minRandRange)) { minRandRange = 5; }
+
   document.getElementById('entrada').value = '';
-  let values = Array(Math.max(Math.floor(Math.random() * 10), 6));
+  let values = Array(maxCount);
+
   for (let i = 0; i < values.length; i++) {
-    values[i] = Math.floor(Math.random() * 50);
+    values[i] = Math.floor(Math.random() * (maxRandRange - minRandRange + 1) + minRandRange);
   }
 
   document.getElementById('entrada').value = values.join(' ');
   document.getElementById('entrada').dispatchEvent(new CustomEvent('change'));
 });
+
+for (let input of ['random_amount', 'max_random_range', 'min_random_range']) {
+  for (let ev of ['change', 'keyup', 'keydown']) {
+    document.getElementById(input).addEventListener(ev, () => {
+      document.getElementById('example_run').dispatchEvent(new CustomEvent('click'));
+    });
+  }
+};
